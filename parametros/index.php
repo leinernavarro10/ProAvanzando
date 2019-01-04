d870b8c61f696f0291bcb8d8c7d20b487876bd65
leinernavarro10 <leinernavarro10@gmail.com>
2019-01-04 17:14:06 -0600

    version 04/01/2018

--- parametros/index.php
+++ parametros/index.php
@@ -6,17 +6,12 @@ if (isset($_POST['guardarLogotipo'])){
 
 	if (is_uploaded_file($_FILES["foto"]["tmp_name"])){
         if ($_FILES["foto"]["type"]=="image/png")    {
-           unlink("../assets/logos/".$_POST['logoViejo']); 
+          
           $info = pathinfo($_FILES["foto"]["name"]);
           $extension = $info['extension'];
           $nombre= "logo".rand(100000, 9999999).".png";
-          $foto2 = "../assets/logos/".$nombre;
-			
-			$sql = "UPDATE tparametros SET logotipo = '$nombre' WHERE id = '".$cuentaid."' ";
-			$query = mysql_query($sql, $conn)or die(mysql_error());
-
+          $foto2 = "../assets/images/logoORI.png";
           move_uploaded_file($_FILES['foto']['tmp_name'], $foto2);
-
         }else{
             $bError = "El formato de la foto tiene que ser PNG.";
         }
@@ -28,16 +23,11 @@ if (isset($_POST['guardarIsotipo'])){
 	if (is_uploaded_file($_FILES["foto"]["tmp_name"])){
         if ($_FILES["foto"]["type"]=="image/png")    {
            
-        	unlink("../assets/logos/".$_POST['isoViejo']); 
           $info = pathinfo($_FILES["foto"]["name"]);
           $extension = $info['extension'];
-          $nombre= "iso".rand(100000, 9999999).".png";
-          $foto2 = "../assets/logos/".$nombre;
-
-          	$sql = "UPDATE tparametros SET isotipo = '$nombre' WHERE id = '".$cuentaid."' ";
-			$query = mysql_query($sql, $conn)or die(mysql_error());
-
+          $foto2 = "../assets/images/isoORI.png";
           move_uploaded_file($_FILES['foto']['tmp_name'], $foto2);
+
         }else{
             $bError = "El formato de la foto tiene que ser PNG.";
         }
@@ -45,61 +35,8 @@ if (isset($_POST['guardarIsotipo'])){
 }
 
 
-if (isset($_POST['guardarFE'])){
 
-	if (is_uploaded_file($_FILES["firma"]["tmp_name"])){
-		
-        if ($_FILES["firma"]["type"]=="application/x-pkcs12")    {
-           unlink("archivos/".$_POST['firmaViejo']); 
-          $info = pathinfo($_FILES["foto"]["name"]);
-          $nombreFirma= "firmaDigital".rand(100000, 9999999);
-          $firma = "archivos/".$nombreFirma.".p12";
-          move_uploaded_file($_FILES['firma']['tmp_name'], $firma);
-         	 $sql = "UPDATE tparametros SET firmador='".$nombreFirma."' WHERE id = '".$cuentaid."' ";
-			$query = mysql_query($sql, $conn)or die(mysql_error());
-        }else{
-            $bError = "El formato del archivo de firma no corresponde.";
-        }
-       
-    }
-     $sql = "UPDATE tparametros SET facturaElectronica='".$_POST['facturaElectronica']."',
-		usuarioFE='".$_POST['usuarioFE']."',
-		pinFE='".$_POST['pinFE']."',
-		passFE='".$_POST['passFE']."',
-		produccion='".$_POST['produccion']."'
-     WHERE id = '".$cuentaid."' ";
-		$query = mysql_query($sql, $conn)or die(mysql_error());
-}
-
-
-if (isset($_POST['guardar'])){
-
-	$sql = "UPDATE tparametros SET 
-	tipocedula='".$_POST['tipocedula']."',
-	cedula='".$_POST['cedula']."',
-	nombre='".$_POST['nombre']."',
-	nomComercial='".$_POST['nomComercial']."',
-	otrasSenas='".$_POST['otrasSenas']."',
-	email='".$_POST['email']."',
-	telefono='".$_POST['telefono']."',
-	leyendaTributaria='".$_POST['leyendaTributaria']."',
-	emailEnvio='".$_POST['emailEnvio']."',
-	factura='".$_POST['factura']."',
-	recibo='".$_POST['recibo']."',
-	facturaGrafica='".$_POST['facturaGrafica']."',
-	tamanoFactura='".$_POST['tamanoFactura']."',
-	imprimirCopia='".$_POST['imprimirCopia']."',
-	estado='1'
-
-	WHERE id = '".$cuentaid."' ";
-	$query = mysql_query($sql, $conn)or die(mysql_error());
-
-	print "<meta http-equiv=\"refresh\" content=\"0;URL=../parametros/\">";
-    exit(); 
-}
-
-
-$pagina="Inicio";
+$pagina="Parámetros";
 $skin='green';
  include("../includes/header.php");
  include("../includes/sidebar.php");
@@ -108,85 +45,53 @@ $skin='green';
 <!-- INICIA CONTENIDO -->
 <ol class="breadcrumb bc-2" >
 	<li><a href="../inicio/"><i class="fa-home"></i>Inicio</a></li>
-	<li><a href="#">Parámetros</a></li>
+	<li><a href="#"><?php echo $pagina?></a></li>
 </ol>
 
-<h2>Parámetros</h2>
+ <h2><?php echo $pagina?></h2>
 <br />
 
 <?php include("../includes/alerts.php");?>
 
 
 <div class="col-md-3">
-	<?php 
-		$sql = "SELECT * FROM tparametros WHERE id = '".$cuentaid."' ";
-		$query = mysql_query($sql, $conn);
-		while ($row=mysql_fetch_assoc($query)) {
-	?>		
 	<div class="panel panel-dark" id="factura">
-		
-		<!-- panel head -->
 		<div class="panel-heading">
 			<div class="panel-title">Logos</div>
-			
 			<div class="panel-options">
 				<a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
 			</div>
 		</div>
-		
-		<!-- panel body -->
-
 		<div class="panel-body">
-		
-							<h3>Logotipo</h3>
-												<form role="form" action="" class="form-horizontal" method="post"  enctype="multipart/form-data">
-												<center>
-<input type="hidden" value="<?php echo $row['logotipo']?>" name="logoViejo">
-												<div style="width: 160px; height: 60px; border: 5px solid #7F7F7F">	
-
-													
-													<?php if (file_exists ('../assets/logos/'.$row['logotipo']) AND $row['logotipo'] !=""){?>
-														<img src="../assets/logos/<?php echo $row['logotipo']?>?<?php echo rand()?>" id="fotoLOGO" width="150" height="50">
-													<?php }else{?>
-														<img src="../assets/images/logoORI.png?<?=rand()?>" id="fotoLOGO" width="150" height="50">
-													<?php } ?>
-					
-												</div>
-												<br>
-												<i>* La imágen debe ser 640 X 200 pixeles para que se mantenga la relación de aspecto.</i>
-												<br>
-												<input type="file" name="foto" onchange="readLOGO(this, 'fotoLOGO');" class="form-control file2 inline btn btn-primary" accept="image/*" >
-												<br><hr>
-												<button type="submit" class="btn btn-green" name="guardarLogotipo">Guardar logotipo</button>
-												</center>
-											</form>
-											
-										<hr>
-
-
-											<h3>Isotipo</h3>
-												<form role="form" action="" class="form-horizontal" method="post"  enctype="multipart/form-data">
-												<center>
-												<input type="hidden" value="<?php echo $row['isotipo']?>" name="isoViejo">
-												<div style="width: 60px; height: 60px; border: 5px solid #7F7F7F">	
-
-													<?php if (file_exists ('../assets/logos/'.$row['isotipo']) AND $row['isotipo'] !=""){?>
-														<img src="../assets/logos/<?php echo $row['isotipo']?>?<?php echo rand()?>" id="fotoLOGO" width="50" height="50">
-													<?php }else{?>
-														<img src="../assets/images/isoORI.png?<?=rand()?>" id="fotoISO" width="50" height="50">
-													<?php } ?>
-					
-												</div>
-												<br>
-												<i>* La imágen debe ser 200 X 200 pixeles para que se mantenga la relación de aspecto.</i>
-												<br>
-												<input type="file" name="foto" onchange="readISO(this, 'fotoISO');" class="form-control file2 inline btn btn-primary" accept="image/*" >
-												<br><hr>
-												<button type="submit" class="btn btn-green" name="guardarIsotipo">Guardar isotipo</button>
-												</center>
-											</form>
-											
-										
+			<h3>Logotipo</h3>
+				<form role="form" action="" class="form-horizontal" method="post"  enctype="multipart/form-data">
+					<center>
+						<div style="width: 160px; height: 60px; border: 5px solid #7F7F7F">	
+							<img src="../assets/images/logoORI.png?<?=rand()?>" id="fotoLOGO" width="150" height="50">
+						</div>
+						<br>
+						<i>* La imágen debe ser 640 X 200 pixeles para que se mantenga la relación de aspecto.</i>
+						<br>
+						<input type="file" name="foto" onchange="readLOGO(this, 'fotoLOGO');" class="form-control file2 inline btn btn-primary" accept="image/*" >
+						<br><hr>
+						<button type="submit" class="btn btn-green" name="guardarLogotipo">Guardar logotipo</button>
+					</center>
+				</form>
+					<hr>
+					<h3>Isotipo</h3>
+					<form role="form" action="" class="form-horizontal" method="post"  enctype="multipart/form-data">
+					<center>
+						<div style="width: 60px; height: 60px; border: 5px solid #7F7F7F">	
+							<img src="../assets/images/isoORI.png?<?=rand()?>" id="fotoISO" width="50" height="50">
+						</div>
+						<br>
+						<i>* La imágen debe ser 200 X 200 pixeles para que se mantenga la relación de aspecto.</i>
+						<br>
+						<input type="file" name="foto" onchange="readISO(this, 'fotoISO');" class="form-control file2 inline btn btn-primary" accept="image/*" >
+						<br><hr>
+						<button type="submit" class="btn btn-green" name="guardarIsotipo">Guardar isotipo</button>
+					</center>
+				</form>							
 		</div>
 	</div>
 
@@ -196,41 +101,15 @@ $skin='green';
 <div class="col-md-9">
 			
 	<div class="panel panel-dark" id="factura">
-		
-		<!-- panel head -->
 		<div class="panel-heading">
 			<div class="panel-title">Parámetros</div>
-			
 			<div class="panel-options">
 				<a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
 			</div>
 		</div>
-		
-		<!-- panel body -->
-
 		<div class="panel-body">
-		
 				<form role="form" action="" class="form-horizontal" method="post">
-
-				<div class="row">
-								<div class="col-md-12">
-									
-									<div class="panel panel-primary" data-collapsed="0">
-									
-										<div class="panel-heading">
-											<div class="panel-title">
-												Datos de la empresa
-											</div>
-											
-											<div class="panel-options">
-												<a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
-											</div>
-										</div>
-										
-										<div class="panel-body">
-								
-
-											<div class="form-group">
+					<div class="form-group">
 											<label for="valor-13" class="col-sm-3 control-label">Tipo de identificación:</label>
 												<div class="col-sm-5">
 													<select name="tipocedula" id="valor-13" class="form-control">
@@ -240,32 +119,15 @@ $skin='green';
 											          <option value="04" <?php if ($row['tipocedula'] == '04'){echo 'selected="selected"';} ?>>NITE</option>
 											        </select>
 												</div>
-											</div>
+					</div>
 											
-											<div class="form-group">
-												<label for="valor-3" class="col-sm-3 control-label">Cédula (Jurídica o Física):</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="cedula" autocomplete="off" id="valor-12" value="<?php echo $row['cedula']?>">
-												</div>
-											</div>
+					<div class="form-group">
+						<label for="valor-3" class="col-sm-3 control-label">Cédula (Jurídica o Física):</label>
+						<div class="col-sm-5">
+							<input type="text" class="form-control" name="cedula" autocomplete="off" id="valor-12" value="<?php echo $row['cedula']?>">
+						</div>
+					</div>
 										
-											<div class="form-group">
-												<label for="valor-3" class="col-sm-3 control-label">Nombre:</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="nombre" autocomplete="off" onblur="javascript: $('#nomComercial').val(this.value)" id="valor-3" value="<?php echo $row['nombre']?>" required>
-												</div>
-											</div>
-												
-											<div class="form-group">
-												<label for="valor-3" class="col-sm-3 control-label">Nombre Comecial:</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="nomComercial" autocomplete="off" id="nomComercial" value="<?php echo $row['nomComercial']?>" required>
-												</div>
-											</div>
-								
 											<hr>											
 											<center>Dirección:</center>
 											<div id="divDireccion" style="border: 1px solid #000000; border-radius: 5px; padding: 10px; margin-bottom: 10px;"></div>
@@ -370,63 +232,11 @@ $skin='green';
 											</div>
 											
 										
-											<div class="form-group">
-												<label for="valor-6" class="col-sm-3 control-label">Email:</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="email" autocomplete="off" id="valor-6" value="<?php echo $row['email']?>">
-												</div>
-											</div>
-								
-											<div class="form-group">
-												<label for="valor-7" class="col-sm-3 control-label">Teléfono:</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="telefono" autocomplete="off" id="valor-7" value="<?php echo $row['telefono']?>">
-												</div>
-											</div>
-							
-											<div class="form-group">
-												<label for="valor-8" class="col-sm-3 control-label">Leyenda tributación:</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="leyendaTributaria" autocomplete="off" id="valor-8" value="<?php echo $row['leyendaTributaria']?>">
-												</div>
-											</div>
-						
-											<div class="form-group">
-												<label for="valor-15" class="col-sm-3 control-label">Dirección de envío de los Emails:</label>
-												
-												<div class="col-sm-5">
-													<input type="text" class="form-control" name="emailEnvio" autocomplete="off" id="valor-15" value="<?php echo $row['emailEnvio']?>">
-												</div>
-											</div>
 					
+				</form>
+		
 											
-										</div>
-									
-									</div>
-								
-								</div>
-							</div>
-				
-
-				<div class="row">
-								<div class="col-md-12">
-									
-									<div class="panel panel-primary" data-collapsed="0">
-									
-										<div class="panel-heading">
-											<div class="panel-title">
-												Números de factura
-											</div>
-											
-											<div class="panel-options">
-												<a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
-											</div>
-										</div>
-										
-										<div class="panel-body">
+							
 								
 											
 
@@ -532,19 +342,8 @@ $skin='green';
 
 											</div>
 
-											
-											
-										</div>
-									
-									</div>
-								
-								</div>
-							</div>
-
-
-			
-
-
+										
+							
 
 					<center>
 					<div class="btn-group">
@@ -557,7 +356,7 @@ $skin='green';
 				</form>
 
 		</div>
-	</div>
+
 
 
 	<div class="panel panel-dark" id="factura">
@@ -656,7 +455,6 @@ $skin='green';
 		</div>
 	</div>
 
-<?php } ?>
 </div>
 
 <script type="text/javascript">
